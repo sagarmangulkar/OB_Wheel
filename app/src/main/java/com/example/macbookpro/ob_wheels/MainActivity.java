dc9a37c1f9e09cf9da9b3070636e53bdc481e18b
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-27 19:22:02 -0400

    Implement new layout for Calender

--- app/src/main/java/com/example/macbookpro/ob_wheels/MainActivity.java
+++ app/src/main/java/com/example/macbookpro/ob_wheels/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.macbookpro.ob_wheels;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Button button1;
+
+    public void init(){
+        button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent firstClick = new Intent(MainActivity.this, CalenderActivity.class);
+                startActivity(firstClick);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
