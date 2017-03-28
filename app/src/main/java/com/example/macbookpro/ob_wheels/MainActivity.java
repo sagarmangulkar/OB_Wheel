34b14a1495355faccd270ab2eb9dd9f5667b76a2
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-28 01:19:09 -0400

    Implement Calender, traditional view and setting EDD layouts with button navigation

--- app/src/main/java/com/example/macbookpro/ob_wheels/MainActivity.java
+++ app/src/main/java/com/example/macbookpro/ob_wheels/MainActivity.java
@@ -9,14 +9,37 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    public Button button1;
+    public Button but1;
+    public Button but2;
+    public Button but3;
 
+    //for first button click
     public void init(){
-        button1 = (Button) findViewById(R.id.button);
-        button1.setOnClickListener(new View.OnClickListener() {
+        but1 = (Button) findViewById(R.id.button1);
+        but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent firstClick = new Intent(MainActivity.this, CalenderActivity.class);
+                startActivity(firstClick);
+            }
+        });
+
+    //for second button click
+        but2 = (Button) findViewById(R.id.button2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent firstClick = new Intent(MainActivity.this, TraditionalWheelActivity.class);
+                startActivity(firstClick);
+            }
+        });
+
+    //for third button click
+        but3 = (Button) findViewById(R.id.button3);
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent firstClick = new Intent(MainActivity.this, SettingEDDActivity.class);
                 startActivity(firstClick);
             }
         });
---
+++ app/src/main/java/com/example/macbookpro/ob_wheels/TraditionalWheelActivity.java
@@ -0,0 +1,13 @@
+package com.example.macbookpro.ob_wheels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TraditionalWheelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traditional_wheel);
+    }
+}
