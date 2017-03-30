a461090b1d329c5702c89b5a03eb2729f2376f5f
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-30 00:55:50 -0400

    Implement Traditional Wheel logic- rotate wheel by button

--- app/src/main/java/com/example/macbookpro/ob_wheels/TraditionalWheelActivity.java
+++ app/src/main/java/com/example/macbookpro/ob_wheels/TraditionalWheelActivity.java
@@ -2,12 +2,36 @@ package com.example.macbookpro.ob_wheels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TraditionalWheelActivity extends AppCompatActivity {
+
+    ImageView imageInnerWheel;
+    ImageView imageOuterWheel;
+    Button buttonRotateRight;
+    int rotationOffSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traditional_wheel);
+        rotateRightWheel();
+    }
+
+    public void rotateRightWheel(){
+        imageInnerWheel = (ImageView)findViewById(R.id.imageInnerWheel);
+        imageOuterWheel = (ImageView)findViewById(R.id.imageOuterWheel);
+        buttonRotateRight = (Button)findViewById(R.id.buttonRotateRight);
+        buttonRotateRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("DEBUG", "Clicked...!");
+                rotationOffSet = rotationOffSet + 5;
+                imageInnerWheel.setRotation(rotationOffSet);
+            }
+        });
     }
 }
