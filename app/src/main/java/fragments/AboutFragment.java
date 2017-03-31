22c5cdeb4a44cf0c8eeb970dcfed848172fe35c5
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-30 21:49:08 -0400

    Add "About" Section contents

--- app/src/main/java/fragments/AboutFragment.java
+++ app/src/main/java/fragments/AboutFragment.java
@@ -17,9 +17,26 @@ public class AboutFragment extends Fragment implements ObFragments {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_ob_landing, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText("AAAA");
+        View rootView = inflater.inflate(R.layout.fragment_text_title_body, container, false);
+        TextView textViewTitle = (TextView) rootView.findViewById(R.id.text_view_title);
+        TextView textViewBody = (TextView) rootView.findViewById(R.id.text_view_body);
+
+        textViewTitle.setText("Development of app");
+        textViewBody.setText("Written to put due date calculator tools and information about " +
+                "setting the due date in the hands of clinicians, students, and residents " +
+                "at the point of care.\n" +
+
+                "\nContent+Design: Joshua Steinberg MD," +
+                "\nUHS Wilson Family Practice Residency," +
+                "\nBinghamton NY\n" +
+                "\nProgrammer: Joshua Steinberg MD;" +
+                "\nHeeRak Kang MSII, SUNY Upstate" +
+                "\nMedical University, Syracuse NY" +
+                "\n" +
+                "Feedback: jds.pocapps@gmail.com\n" +
+                "Platform: iOS 7.0 and later\n" +
+                "Version: 1.0\n" +
+                "Copyright: March, 2015");
         return rootView;
     }
 
