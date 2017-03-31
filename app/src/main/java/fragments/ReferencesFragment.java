43fcf5fc05a77ff67a8feb3c23e33d06e897cd10
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-30 21:53:49 -0400

    Add "References" Section contents

--- app/src/main/java/fragments/ReferencesFragment.java
+++ app/src/main/java/fragments/ReferencesFragment.java
@@ -17,9 +17,18 @@ public class ReferencesFragment extends Fragment implements ObFragments {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_ob_landing, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText("AAAA");
+        View rootView = inflater.inflate(R.layout.fragment_text_title_body, container, false);
+        TextView textViewTitle = (TextView) rootView.findViewById(R.id.text_view_title);
+        TextView textViewBody = (TextView) rootView.findViewById(R.id.text_view_body);
+
+        textViewTitle.setText("References");
+        textViewBody.setText("Method for Estimating Due Date.  American College of Obstetricians and Gynecologists (ACOG).  Committee Opinion Number 611.  Obst Gyn vol. 124, no. 4, October, 2014.\n" +
+                "\n" +
+                "Bennett KA, et. al. First trimester ultrasound screening is effective in reducing postterm labor induction rates: a randomized controlled trial. Am J Obst Gyn 2004;190:1077–81.\n" +
+                "\n" +
+                "Whitworth M, et. al. Ultrasound for fetal assessment in early pregnancy. Cochrane Database of Systematic Reviews 2010, Issue 4. Art. No.: CD007058.\n" +
+                "\n" +
+                "Wegienka G, Baird DD. A comparison of recalled date of last menstrual period with prospectively recorded dates. J Womens Health 2005;14:248–52.");
         return rootView;
     }
 
