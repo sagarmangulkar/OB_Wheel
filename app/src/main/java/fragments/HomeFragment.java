6dac0cc120dcb38ae7581e928234a72f2d9d9e7c
Sagar Mangulkar <smangul1@binghamton.edu>
2017-03-30 21:59:00 -0400

    Refactor MainActivity to LandingActivity

--- app/src/main/java/fragments/HomeFragment.java
+++ app/src/main/java/fragments/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment implements ObFragments, View.OnClickL
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.activity_main, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
         init();
         return rootView;
     }
